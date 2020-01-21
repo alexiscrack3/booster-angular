4ee266e230a3123605a73676aac81a22c749fb33
Alexis Ortega <alexis.oc3@gmail.com>
2020-01-20 17:09:32 -0700

    Add entries component

---
+++ src/app/entries/entries.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Entry } from '../models/entry';
+
+@Component({
+  selector: 'app-entries',
+  templateUrl: './entries.component.html',
+  styleUrls: ['./entries.component.scss']
+})
+export class EntriesComponent implements OnInit {
+  entries: Entry[] = [
+    {
+      id: '1',
+      class: 'noun',
+      headword: 'soccer'
+  }, {
+    id: '2',
+    class: 'verb',
+    headword: 'run'
+  }, {
+    id: '3',
+    class: 'noun',
+    headword: 'climbing'
+  }
+];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/models/entry.ts
@@ -0,0 +1,5 @@
+export class Entry {
+    id: string;
+    class: string;
+    headword: string;
+  }
